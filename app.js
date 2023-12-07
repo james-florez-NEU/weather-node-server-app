62a940ebb16e7f6d63394e982963a2b00a50fcdb
James <jamesflorez11@gmail.com>
2023-12-07 13:42:49 -0800

    update app.js

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 import express from "express";
 import session from "express-session";
 import "dotenv/config";
-import Hello from "./hello.js";
-import Lab5 from "./lab5.js";
-import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
+import Weather from "./weather.js";
 
-const CONNECTION_STRING = "mongodb+srv://florezja:JlJZWqSQzBY282S4@cluster0.y6pwqaq.mongodb.net/kanbas?retryWrites=true&w=majority";
-mongoose.connect(CONNECTION_STRING);
+mongoose.connect(process.env.DB_CONNECTION_STRING);
 const app = express();
 app.use(
     cors({
@@ -37,6 +33,5 @@ app.use(express.json());
 UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
-Lab5(app);
-Hello(app);
+
 app.listen(process.env.PORT || 4000);
