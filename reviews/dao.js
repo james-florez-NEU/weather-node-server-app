f4154ca8693b6266d4a38d79e0123447c69ed23d
James <jamesflorez11@gmail.com>
2023-12-09 14:38:32 -0800

    Implement findReviewById()

--- reviews/dao.js
+++ reviews/dao.js
@@ -1,3 +1,4 @@
 import model from "./model.js";
 export const findAllReviews = () => model.find();
 export const findNewestReviews = () => model.find().sort({ date: -1 }).limit(2);
+export const findReviewById = (reviewId) => model.findById(reviewId);
--- reviews/routes.js
+++ reviews/routes.js
@@ -8,9 +8,13 @@ function ReviewRoutes(app) {
         const reviews = await dao.findNewestReviews();
         res.json(reviews);
     };
-
+    const findReviewById = async (req, res) => {
+        const review = await dao.findReviewById(req.params.reviewId);
+        res.json(review);
+    }
 
     app.get("/api/reviews", findAllReviews);
     app.get("/api/reviews/newest", findNewestReviews);
+    app.get("/api/reviews/:reviewId", findReviewById);
 }
 export default ReviewRoutes;
