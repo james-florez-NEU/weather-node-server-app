1af347884c1f5cd9a1a41a1698170ed4b770120d
James <jamesflorez11@gmail.com>
2023-12-08 16:36:21 -0800

    Add date to review schema and ability to find the 2 newest reviews.

--- reviews/dao.js
+++ reviews/dao.js
@@ -1,2 +1,3 @@
 import model from "./model.js";
 export const findAllReviews = () => model.find();
+export const findNewestReviews = () => model.find().sort({ date: -1 }).limit(2);
--- reviews/routes.js
+++ reviews/routes.js
@@ -4,7 +4,13 @@ function ReviewRoutes(app) {
         const reviews = await dao.findAllReviews();
         res.json(reviews);
     };
+    const findNewestReviews = async (req, res) => {
+        const reviews = await dao.findNewestReviews();
+        res.json(reviews);
+    };
+
 
     app.get("/api/reviews", findAllReviews);
+    app.get("/api/reviews/newest", findNewestReviews);
 }
 export default ReviewRoutes;
