d306e10ef82f922f4aac7a655af55add14e12200
James <jamesflorez11@gmail.com>
2023-12-09 15:41:56 -0800

    Implement findChannelById()

--- channels/dao.js
+++ channels/dao.js
@@ -1,2 +1,3 @@
 import model from "./model.js";
 export const findAllChannels = () => model.find();
+export const findChannelById = (channelId) => model.findById(channelId);
--- channels/routes.js
+++ channels/routes.js
@@ -4,7 +4,12 @@ function ChannelRoutes(app) {
         const channels = await dao.findAllChannels();
         res.json(channels);
     };
+    const findChannelById = async (req, res) => {
+        const channel = await dao.findChannelById(req.params.channelId);
+        res.json(channel);
+    }
 
     app.get("/api/channels", findAllChannels);
+    app.get("/api/channels/:channelId", findChannelById);
 }
 export default ChannelRoutes;
