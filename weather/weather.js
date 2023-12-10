b30cb5c435e6fac2b1f023ccd2b1ebccaee24634
James <jamesflorez11@gmail.com>
2023-12-10 10:26:47 -0800

    Clean up weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -8,11 +8,8 @@ const Weather = (app) => {
     app.get('/api/weather/current/:id', async (req, res) => {
         const { id } = req.params;
         const api_string = WEATHER_API_BASE + 'current.json?key=' + WEATHER_API_KEY + '&q=id:' + id;
-        console.log(api_string);
         try {
-
             const response = await axios.get(api_string);
-
             const weatherData = response.data;
             res.json(weatherData);
         } catch (error) {
@@ -22,32 +19,6 @@ const Weather = (app) => {
     app.get('/api/weather/search/:key', async (req, res) => {
         const { key } = req.params;
         const api_string = WEATHER_API_BASE + 'search.json?key=' + WEATHER_API_KEY + '&q=' + key;
-        try {
-            const response = await axios.get(api_string);
-            const weatherData = response.data;
-            res.json(weatherData);
-        } catch (error) {
-            res.status(500).json({ error: error.toString() });
-        }
-    });
-
-
-
-    app.get('/api/weather/current/test', async (req, res) => {
-        const api_string = WEATHER_API_BASE + 'current.json?key=' + WEATHER_API_KEY + '&q=London';
-        console.log(api_string);
-        try {
-
-            const response = await axios.get(api_string);
-
-            const weatherData = response.data;
-            res.json(weatherData);
-        } catch (error) {
-            res.status(500).json({ error: "current error"});
-        }
-    });
-    app.get('/api/weather/search/test', async (req, res) => {
-        const api_string = WEATHER_API_BASE + 'search.json?key=' + WEATHER_API_KEY + '&q=London';
         try {
             const response = await axios.get(api_string);
             const weatherData = response.data;
