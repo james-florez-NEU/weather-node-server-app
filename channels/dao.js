663cd446572d3b5a4d2a3fd429f58b72ae5f3ff7
James <jamesflorez11@gmail.com>
2023-12-09 23:26:12 -0800

    Implement updating channel

--- channels/dao.js
+++ channels/dao.js
@@ -1,3 +1,4 @@
 import model from "./model.js";
 export const findAllChannels = () => model.find();
 export const findChannelById = (channelId) => model.findById(channelId);
+export const updateChannel = (channelId, channel) => model.updateOne({ _id: channelId }, { $set: channel });
--- channels/routes.js
+++ channels/routes.js
@@ -8,7 +8,13 @@ function ChannelRoutes(app) {
         const channel = await dao.findChannelById(req.params.channelId);
         res.json(channel);
     }
+    const updateChannel = async (req, res) => {
+        const { channelId } = req.params;
+        const status = await dao.updateChannel(channelId, req.body);
+        res.json(status);
+    }
     app.get("/api/channels", findAllChannels);
     app.get("/api/channels/:channelId", findChannelById);
+    app.put("/api/channels/:channelId", updateChannel);
 }
 export default ChannelRoutes;
