ba8a7ded8782a134f8e1bc7570aba590df567354
James <jamesflorez11@gmail.com>
2023-12-10 10:31:18 -0800

    Add favorites and favoriteChannels to user schema

--- users/schema.js
+++ users/schema.js
@@ -10,6 +10,8 @@ const userSchema = new mongoose.Schema({
             type: String,
             enum: ["USER", "FORECASTER", "MODERATOR"],
             default: "USER" },
+        favorites: [String],
+        favoriteChannels: [String],
     },
     { collection: "users" });
 export default userSchema;
