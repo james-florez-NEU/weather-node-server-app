59f3cf97bfcda685bac7621c63504f3cd7002383
James <jamesflorez11@gmail.com>
2023-12-09 22:04:39 -0800

    Implement creating and deleting reviews

--- reviews/dao.js
+++ reviews/dao.js
@@ -1,4 +1,6 @@
 import model from "./model.js";
 export const findAllReviews = () => model.find();
-export const findNewestReviews = () => model.find().sort({ date: -1 }).limit(2);
+export const findNewestReviews = () => model.find().sort({ date: -1 }).limit(3);
 export const findReviewById = (reviewId) => model.findById(reviewId);
+export const createReview = (review) => model.create(review);
+export const deleteReview = (reviewId) => model.deleteOne({ _id: reviewId });
--- reviews/routes.js
+++ reviews/routes.js
@@ -12,9 +12,19 @@ function ReviewRoutes(app) {
         const review = await dao.findReviewById(req.params.reviewId);
         res.json(review);
     }
+    const createReview = async (req, res) => {
+        const review = await dao.createReview(req.body);
+        res.json(review);
+    }
+    const deleteReview = async (req, res) => {
+        const status = await dao.deleteReview(req.params.reviewId);
+        res.json(status);
+    }
 
     app.get("/api/reviews", findAllReviews);
     app.get("/api/reviews/newest", findNewestReviews);
     app.get("/api/reviews/:reviewId", findReviewById);
+    app.post("/api/reviews/create", createReview);
+    app.delete("/api/reviews/:reviewId", deleteReview);
 }
 export default ReviewRoutes;
